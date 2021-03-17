881a7b8ffa5d70028f80b31db878b111ad3228b9
Shiroikoi <48037748+Shiroikoi@users.noreply.github.com>
2021-03-17 20:50:35 +0800

    init

---
+++ src/components/AppBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app color="#616161" dark>
+    <v-img :src="src" max-width="100"> </v-img>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: "AppBar",
+    data: () => ({
+      src: require("../assets/ros_logo_white.png"),
+    }),
+  };
+</script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" app permanent>
+    <v-list-item class="px-2">
+      <v-list-item-avatar>
+        <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-title>John Leider</v-list-item-title>
+
+      <v-btn icon @click.stop="mini = !mini">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+    </v-list-item>
+  </v-navigation-drawer>
+</template>
+<script>
+  export default {
+    name: "NavigationDrawer",
+    data: function() {
+      return { drawer: true, mini: false };
+    },
+  };
+</script>
