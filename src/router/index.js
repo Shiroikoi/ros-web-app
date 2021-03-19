cdfc61c45bd4ed0fd419ee9e935957fc5ad161f6
Shiroikoi <48037748+Shiroikoi@users.noreply.github.com>
2021-03-19 09:40:47 +0800

    add axios

--- src/router/index.js
+++ src/router/index.js
@@ -5,40 +5,51 @@ import About from "../views/About";
 import TODO1 from "../views/TODO1";
 import TODO2 from "../views/TODO2";
 import Index from "../views/Index";
+import Login from "../views/Login";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-
+    name: "Index",
     component: Index,
   },
   {
     path: "/ControlPanel",
-
+    name: "ControlPanel",
     component: ControlPanel,
   },
   {
     path: "/About",
-
+    name: "About",
     component: About,
   },
   {
     path: "/TODO1",
-
+    name: "TODO1",
     component: TODO1,
   },
   {
     path: "/TODO2",
-
+    name: "TODO2",
     component: TODO2,
+  },
+  {
+    path: "/Login",
+    name: "Login",
+    component: Login,
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== "Login" && true) next({ name: "Login" });
+  else next();
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>This is a Login page</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: "Login",
+  };
+</script>
